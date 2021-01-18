b53833927fb770b8e16ae51d548fe0137b6731e6
SoHardToMakeAName <36100290+SoHardToMakeAName@users.noreply.github.com>
2021-01-18 12:04:40 +0800

    Add filters and noises

--- exp3/ops.py
+++ exp3/ops.py
@@ -1,7 +1,10 @@
 from exp1.bitmap import Bitmap
 from exp1.converter import Converter
+from exp2.dct import compute_psnr
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import convolve2d
+from skimage.measure import compare_ssim
 
 class Operations:
     def __init__(self):
@@ -12,14 +15,15 @@ class Operations:
         # gray：值域为[0.255]的灰度图像；can：直方图的区间个数
         step = int(np.ceil(256 / can))
         prob = [0 for _ in range(can)]
+        num = [0 for _ in range(can)]
         h, w = gray.shape[0], gray.shape[1]
         for i in range(h):
             for j in range(w):
                 pix_val = gray[i, j]
-                prob[pix_val//step] += 1
+                num[pix_val//step] += 1
         for i in range(len(prob)):
-            prob[i] = prob[i] / (h*w)
-        return prob
+            prob[i] = num[i] / (h*w)
+        return prob, num
 
     @staticmethod
     def hist_normalization(gray, min_max=[0, 255], can_num=256):
@@ -30,8 +34,8 @@ class Operations:
         for i in range(h):
             for j in range(w):
                 data[i, j] = (tar_max - tar_min) / (pix_max - pix_min) * (data[i, j] - pix_min) + tar_min
-        prob_before = Operations.hist(gray, can_num)
-        prob_after = Operations.hist(data, can_num)
+        prob_before, _ = Operations.hist(gray, can_num)
+        prob_after, _ = Operations.hist(data, can_num)
         plt.subplot(2, 2, 1)
         plt.bar(range(len(prob_before)), prob_before)
         plt.xlabel("pixel value")
@@ -55,7 +59,7 @@ class Operations:
     def hist_equalization(gray, can_num=256):
         data = gray.copy()
         h, w = gray.shape[0], gray.shape[1]
-        hist_before = Operations.hist(gray, can=can_num)
+        hist_before, _ = Operations.hist(gray, can=can_num)
         step = int(np.ceil(256 / can_num))
         acc_prob = [0 for _ in range(can_num)]
         acc_prob[0] = hist_before[0]
@@ -91,12 +95,117 @@ class Operations:
         plt.show()
         return data
 
+    @staticmethod
+    def add_guassian_noise(img, sigma):
+        noise = np.random.normal(0.0, sigma, img.shape)
+        img_ = (img + noise).round().clip(0, 255).astype(np.uint8)
+        return img_
+
+    @staticmethod
+    def add_salty_noise(img, SNR):
+        img_ = img.copy()
+        h, w = img_.shape
+        mask = np.random.choice((0, 1, 2), size=(h, w), p=[SNR, (1 - SNR) / 2., (1 - SNR) / 2.])
+        img_[mask == 1] = 255
+        img_[mask == 2] = 0
+        return img_.round().clip(0, 255).astype(np.uint8)
+
+    @staticmethod
+    def convolve2d(img, kernel, mode='default'):
+        kh, kw = kernel.shape[0], kernel.shape[1]
+        ih, iw = img.shape[0], img.shape[1]
+        if len(img.shape) == 2:
+            ic = 1
+            img_ = np.expand_dims(img.copy(), 2)
+        else:
+            ic = img.shape[2]
+            img_ = img.copy()
+        if len(kernel.shape) == 2:
+            kernel = np.repeat(np.expand_dims(kernel, 2), ic, axis=2)
+        elif kernel.shape[2] != ic:
+            raise Exception("Channel Mismatch")
+        if mode == 'same':
+            img_ = np.pad(img_, ((kh//2, kh//2), (kw//2, kw//2), (0, 0)), 'constant')
+            res = np.zeros((ih, iw))
+        else:
+            res = np.zeros((ih-kh+1, iw-kw+1, ic))
+        for i in range(res.shape[0]):
+            for j in range(res.shape[1]):
+                patch = img_[i:i+kh, j:j+kh]
+                res[i, j] = np.sum(patch * kernel)
+        return res.round().clip(0, 255).astype(np.uint8)
+
+    @staticmethod
+    def mean_filter(img, kernel_size):
+        kernel = (1/kernel_size**2) * np.ones((kernel_size, kernel_size))
+        return Operations.convolve2d(img, kernel, 'same')
+
+    @staticmethod
+    def median_filter(img, kernel_size):
+        img_ = np.pad(img.copy(), ((kernel_size // 2, kernel_size // 2), (kernel_size // 2, kernel_size // 2)), 'constant')
+        res = np.zeros(img.shape)
+        for i in range(res.shape[0]):
+            patch = img_[i:i + kernel_size, 0:kernel_size]
+            _, hist = Operations.hist(patch)
+            mdn = np.sum([i*hist[i] for i in range(256)]) / (kernel_size**2)
+            mdn = int(np.floor(mdn))
+            Ltmdn = int(np.sum(patch<mdn))
+            res[i, 0] = mdn
+            for j in range(1, res.shape[1]):
+                left = img_[i:i+kernel_size, j-1]
+                right = img_[i:i+kernel_size, j+kernel_size-1]
+                for pix in left:
+                    hist[pix] -= 1
+                    if pix < mdn:
+                        Ltmdn -= 1
+                for pix in right:
+                    hist[pix] += 1
+                    if pix < mdn:
+                        Ltmdn += 1
+                th = int((kernel_size**2-1)/2)
+                while Ltmdn > th:
+                    mdn = mdn - 1
+                    Ltmdn = Ltmdn - hist[mdn]
+                while Ltmdn + hist[mdn] <= th:
+                    Ltmdn += hist[mdn]
+                    mdn += 1
+                res[i, j] = mdn
+        return res
+
+
+
+
+def compute_ssim(im1, im2):
+    isRGB = len(im1.shape) == 3 and im1.shape[-1] == 3
+    s = compare_ssim(im1, im2, K1=0.01, K2=0.03, gaussian_weights=True, sigma=1.5, use_sample_covariance=False,
+             multichannel=isRGB)
+    return s
+
 
 def test(file_path):
     img = Bitmap(file_path)
-    test_data = img.get_data()[:, :, 0]
-    test_data_1 = np.ceil(test_data / 255.0 * 100).astype(np.uint8)
+    rgb_data = img.get_data()
+    test_data = Converter.rgb2ycbcr(rgb_data)[:, :, 0]
+    # test_data_1 = np.ceil(test_data / 255.0 * 100).astype(np.uint8)
     # Operations.hist_normalization(test_data_1)
-    Operations.hist_equalization(test_data_1)
+    # Operations.hist_equalization(test_data_1)
+    # img_g = Operations.add_guassian_noise(test_data, 25)
+    # img_g_filtered = Operations.mean_filter(test_data, 3)
+    # plt.subplot(1, 2, 1)
+    # plt.imshow(img_g, cmap=plt.cm.gray)
+    # plt.subplot(1, 2, 2)
+    # plt.imshow(img_g_filtered, cmap=plt.cm.gray)
+    # plt.show()
+    # print("Mean Filter: PSNR={}, SSIM={}".format(compute_psnr(img_g, img_g_filtered),
+    #                                              compare_ssim(img_g, img_g_filtered)))
+    img_s = Operations.add_salty_noise(test_data, 0.9)
+    img_s_filtered = Operations.median_filter(img_s, 3)
+    plt.subplot(1, 2, 1)
+    plt.imshow(img_s, cmap=plt.cm.gray)
+    plt.subplot(1, 2, 2)
+    plt.imshow(img_s_filtered, cmap=plt.cm.gray)
+    plt.show()
+    print("Mean Filter: PSNR={}, SSIM={}".format(compute_psnr(img_s, img_s_filtered),
+                                                 compare_ssim(img_s, img_s_filtered)))
 
 test("../exp1/lena512color.bmp")
